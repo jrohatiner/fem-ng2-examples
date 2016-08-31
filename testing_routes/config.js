1dea7ac69a6753d3d34fa0f0a227eb23783f0935
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-08-31 14:26:39 -0700

    update all configs to rc5

--- testing_routes/config.js
+++ testing_routes/config.js
@@ -1,16 +1,13 @@
 (function(global) {
 
-  var ngVer = '@2.0.0-rc.3'; // lock in the angular package version; do not let it float to current!
+  var ngVer = '@2.0.0-rc.5', // lock in the angular package version; do not let it float to current!
+    formVer = '@0.3.0',
+    routerVer = '@3.0.0-rc.1',
+    materialVer = '@2.0.0-alpha.7-4';
 
   //map tells the System loader where to look for things
   var  map = {
     'app':                        'src', // 'dist',
-    '@angular/router':            'https://npmcdn.com/@angular/router@3.0.0-alpha.7',
-    'ng2-material':               'https://npmcdn.com/ng2-material@0.5.0',
-    '@angular2-material/core':    'https://npmcdn.com/@angular2-material/core@2.0.0-alpha.5',
-    // For some reason, if we don't include the checkbox code the app dies
-    '@angular2-material/checkbox':'https://npmcdn.com/@angular2-material/checkbox@2.0.0-alpha.5',
-    '@angular2-material/tabs':'https://npmcdn.com/@angular2-material/tabs@2.0.0-alpha.5',
     'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
     'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api' // get latest
   };
@@ -18,35 +15,40 @@
   //packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.ts',  defaultExtension: 'ts' },
-    '@angular/router':            { main: 'index.js', defaultExtenstion: 'js' },
-    'ng2-material':               { main: 'index.js', defaultExtension: 'js' },
-    '@angular2-material/core':    { format: 'cjs', defaultExtension: 'js', main: 'core.js' },
-    '@angular2-material/tabs':    { format: 'cjs', defaultExtension: 'js', main: 'tabs.js' },
     'rxjs':                       { defaultExtension: 'js', main: 'Rx.js' },
-    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { defaultExtension: 'js' }
   };
 
   var packageNames = [
-      '@angular/common',
-      '@angular/compiler',
-      '@angular/core',
-      '@angular/http',
-      // '@angular/router',
-      '@angular/platform-browser',
-      '@angular/platform-browser-dynamic',
-      '@angular/router-deprecated',
-      '@angular/testing',
-      '@angular/upgrade',
+    '@angular2-material/core',
+    '@angular2-material/tabs',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/testing',
+    '@angular/upgrade'
   ];
 
   // add map entries for angular packages in the form '@angular/common': 'https://npmcdn.com/@angular/common@0.0.0-3'
   packageNames.forEach(function(pkgName) {
-    map[pkgName] = 'https://npmcdn.com/' + pkgName + ngVer;
+    var ver = pkgName.includes('forms') ? formVer
+      : pkgName.includes('router') ? routerVer
+      : pkgName.includes('angular2-material') ? materialVer
+      : ngVer;
+
+    map[pkgName] = 'https://npmcdn.com/' + pkgName + ver;
   });
 
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
   packageNames.forEach(function(pkgName) {
-    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    var format = pkgName.includes('angular2-material')
+      ? { format: 'cjs', defaultExtension: 'js', main: pkgName.split('/')[1] + '.js' }
+      : { main: 'index.js', defaultExtension: 'js' };
+
+    packages[pkgName] = format;
   });
 
   var config = {
@@ -56,7 +58,7 @@
     },
     map: map,
     packages: packages
-  }
+  };
 
   // filterSystemConfig - index.html's chance to modify config before we register it.
   if (global.filterSystemConfig) { global.filterSystemConfig(config); }
